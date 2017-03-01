7a401b4a18bae2d015020a9919bf43d46c240e5f
Brian Cooney <briancooney@Rorys-MBP.home>
2017-03-01 16:51:16 +0000

    established card variables

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,11 @@
 console.log("JS file is connected to HTML! Woo!")
+
+
+/* Create a variable for each memory game card */
+var cardOne = "queen"; 
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+
+
