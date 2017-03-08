08bffda0f04033754b3840ee195be2726a8901a1
Brian Cooney <briancooney@Rorys-MBP.home>
2017-03-08 18:00:53 +0000

    completed memory card game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,33 +1,59 @@
-console.log("JS file is connected to HTML! Woo!")
 
 
-/* Create a variable for each memory game card */
-var cardOne = "queen"; 
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// 1. set up variable with the cards in the game
+var cards = ['queen', 'queen', 'king', 'king'];
+
+// 2. create empth variable to add values into during play
+var cardsInPlay = [];
 
 
-var createCards = function() {
+// 3. find the html div tag with id "game-board"
+var board = document.getElementById("game-board");
+//the following function will create the 4 cards **creating new <Div>'s  
+function createBoard() {
 
-var gameBoard = document.getElementById('game-board');
+	for(var i = 0; i < cards.length; i++){ 
+       var cardElement = document.createElement('div');
+       cardElement.className = 'card';
+       cardElement.setAttribute('data-card', cards[i]);
+       // 4. add click event listner
+       cardElement.addEventListener('click', isTwoCards);
+	   board.appendChild(cardElement);
+	   // Trouble shooting relationship of cardsInPlay array
+	   console.log(cardsInPlay); 
+   
+  }     
+} 
 
-for (var i = 0; i < cards.length; i++) {
-		var cardDiv = document.createElement('div');
-		cardDiv.className = 'card';
-}
-gameBoard.appendChild(cardDiv);
+function isTwoCards() {
+    cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='king.png' alt='King'>"; // king locally stored img
+	} else {
+		this.innerHTML = "<img src='queen.png' alt='Queen'>"; //queen locally stored img
 	}
-}
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    // clear cards in play array for next try.
+    cardsInPlay = [];
+  }
 }
 
-/*
-if (cardOne === cardTwo) {
-	alert("You found a match!");
-} else if (cardThree === cardFour) {
-	alert("You found a match!");
-} else {
-	alert("sorry, try again.");
+// 5. Create function to test if two cards are a match
+function isMatch(cards) {
+  // 0 & 1 point to the first to values in the cards array "queen", "queen"
+    if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  
+  }
 }
-*/
+// Run functions
+createBoard();
+// do these functions have to be called? 
+isTwoCards(); 
+isMatch();
+
 
